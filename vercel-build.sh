824f3bf8c9237df43ccf4f37cfe924f0873ed942
bebaskreatif99 <bebaskreatif.99@gmail.com>
2025-10-07 16:35:25 +0700

    Menggunakan python3.9 di skrip build

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+# Menggunakan python3.9 (versi stabil di Vercel) untuk menginstal dependensi
+python3.9 -m pip install -r requirements.txt
 
-python -m flask db upgrade
+# Menggunakan python3.9 untuk menjalankan migrasi database
+python3.9 -m flask db upgrade
