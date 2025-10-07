081b953e3e5d5b33f0d40593dbd2ebe7b05b8b7d
bebaskreatif99 <bebaskreatif.99@gmail.com>
2025-10-07 16:44:05 +0700

    Menyederhanakan konfigurasi Vercel

--- vercel-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Menggunakan python3.9 (versi stabil di Vercel) untuk menginstal dependensi
-python3.9 -m pip install -r requirements.txt
-
-# Menggunakan python3.9 untuk menjalankan migrasi database
-python3.9 -m flask db upgrade
